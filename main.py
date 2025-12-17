19d88d627c71e4552eb30562f221edec89083877
Daniel-Duque <68342851+Daniel-Duque@users.noreply.github.com>
2025-12-17 14:53:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import streamlit
+import streamlit as st
 data=pd.read_excel(r"https://d1b4gd4m8561gs.cloudfront.net/sites/default/files/caracteristicas-financieras-titulos-vigentes.xlsx",skiprows=3)
 suma=data[data["Categoría del Título"]=="TIDIS"]["Saldo en circulación"].sum()
 st.write(f"Suma TIDIS: ${suma:,.2f}")
