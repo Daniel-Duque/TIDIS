2240d5f4b51b445fe700dad02c82679c4ddee0a0
Daniel-Duque <68342851+Daniel-Duque@users.noreply.github.com>
2025-12-18 11:03:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ suma=data[data["Categoría del Título"]=="TIDIS"]["Saldo en circulación"].sum(
 st.write(f"Suma TIDIS: ${suma:,.2f}")
 data["Mes de vencimiento"]=data["Fecha de vencimiento"]
 plot=data[data["Categoría del Título"]=="TIDIS"][["Mes de vencimiento","Saldo en circulación"]].groupby(pd.Grouper(key="Mes de vencimiento", freq='M')).sum().plot.bar()
-st.pyplot(plot)
+st.pyplot(plot.figure)
