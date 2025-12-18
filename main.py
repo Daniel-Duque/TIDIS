53cd72df80f49b3ef0f084c963c6e9d9deb4c958
Daniel-Duque <68342851+Daniel-Duque@users.noreply.github.com>
2025-12-18 11:02:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 data=pd.read_excel(r"https://d1b4gd4m8561gs.cloudfront.net/sites/default/files/caracteristicas-financieras-titulos-vigentes.xlsx",skiprows=3)
 suma=data[data["Categoría del Título"]=="TIDIS"]["Saldo en circulación"].sum()
 st.write(f"Suma TIDIS: ${suma:,.2f}")
+data["Mes de vencimiento"]=data["Fecha de vencimiento"]
+plot=data[data["Categoría del Título"]=="TIDIS"][["Mes de vencimiento","Saldo en circulación"]].groupby(pd.Grouper(key="Mes de vencimiento", freq='M')).sum().plot.bar()
+st.pyplot(plot)
