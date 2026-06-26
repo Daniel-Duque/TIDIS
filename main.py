dd7fccd06f5cd28fd617a43ed3a05d89781a0719
Daniel-Duque <68342851+Daniel-Duque@users.noreply.github.com>
2026-06-26 09:55:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ data=pd.read_excel(r"https://d1b4gd4m8561gs.cloudfront.net/sites/default/files/c
 suma=data[data["Categoría del Título"]=="TIDIS"]["Saldo en circulación"].sum()
 st.write(f"Suma TIDIS: ${suma:,.2f}")
 data["Mes de vencimiento"]=data["Fecha de vencimiento"]
-plot=data[data["Categoría del Título"]=="TIDIS"][["Mes de vencimiento","Saldo en circulación"]].groupby(pd.Grouper(key="Mes de vencimiento", freq='M')).sum().plot.bar()
+plot=data[data["Categoría del Título"]=="TIDIS"][["Mes de vencimiento","Saldo en circulación"]].groupby(pd.Grouper(key="Mes de vencimiento", freq='ME')).sum().plot.bar()
 st.pyplot(plot.figure)
